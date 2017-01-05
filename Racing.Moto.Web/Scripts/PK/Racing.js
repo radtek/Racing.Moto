fb9cddd77819f21216f20fd0f23d9b18e03d4c80
wdyday <wdyday@163.com>
2017-01-05 23:13:28 +0800

    20170105

--- Racing.Moto.Web/Scripts/PK/Racing.js
+++ Racing.Moto.Web/Scripts/PK/Racing.js
@@ -29,7 +29,7 @@
 
         //$stockTable.append("aaa");
 
-        pkInfo.PK = { PKId: 1 };
+        pkInfo.PK = { PKId: 1, Ranks: '3,2,5,6,8,7,10,1,9,4', GameSeconds: 30 };
 
         motoRacing.run(pkInfo);
     }
@@ -41,30 +41,72 @@
     // moto
     var motoRacing = {
         PKInfo: null,
+        Colors: ['red', 'blue', 'yellow', 'green', 'gray', 'aqua', 'blueviolet', 'brown', 'Highlight', 'aquamarine', 'teal'],
+        //Easings: ['easeInQuad', 'easeInQuart', 'easeInOutSine', 'easeOutSine', 'easeOutQuad', 'linear', 'easeInCirc', 'easeInOutQuad', 'easeOutCubic', 'easeInOutCubic'],
+        Easings: ['easeInOutSine', 'easeOutSine', 'linear', 'swing', 'easeInOutSine', 'easeInOutQuad', 'swing', 'easeInOutSine', 'easeOutSine', 'easeInOutQuad'],
         run: function (pkInfo) {
             // new racing
             if (motoRacing.PKInfo == null || pkInfo.PK.PKId != motoRacing.PKInfo.PK.PKId) {
                 motoRacing.PKInfo = pkInfo;
 
                 // road moving
-                $("#roadBg").floatingBg({ direction: 5, speed: 5 });
+                $("#roadBg").floatingBg({ direction: 5, speed: 5, backgroud: '/images/road.png', });
 
+                // moto append
+                motoRacing.append();
                 // moto moving
-
+                motoRacing.motoRun();
             }
-
-            //test
-            //$("#roadBg").floatingBg("destroy");
-            //$("#roadBg").floatingBg({ direction: 5, speed: 5 });
         },
-        calculateSpeed: function (ranks) {
+        append: function () {
+            var html = '';
+            for (var i = 0; i < 10; i++) {
+                var top = (100 + 20 * i) + 'px;';
+                var bg = motoRacing.Colors[i];
+                html += '<div id="moto' + (i + 1) + '" class="moto" style="top:' + top + ' background-color:' + bg + '"></div>';
+            }
+            $('#motos').html(html);
+        },
+        motoRun: function () {
+            var speeds = motoRacing.calculateSpeeds();
+            // run
+            for (var i = 0; i < speeds.length; i++) {
+                var param = speeds[i].Rank != 10
+                    ? { duration: speeds[i].Duration, easing: speeds[i].Easing }
+                    : {
+                        duration: speeds[i].Duration, easing: speeds[i].Easing, complete: function () {
+                            $("#roadBg").floatingBg("destroy");
+                        }
+                    };
+
+                $('#moto' + (i + 1)).animate({ right: 1000 }, param);
+            }
+        },
+        calculateSpeeds: function () {
+            var speeds = [];
+
             var pk = motoRacing.PKInfo.PK;
 
             //ranks: 3,2,5,6,8,7,10,1,9,4
             var rankArr = pk.Ranks.split(',');
-            //var 
 
+            // speeds
+            for (var i = 0; i < rankArr.length; i++) {
+                var num = motoRacing.getRandomNum(0, 9);
+                speeds.push({
+                    'Rank': rankArr[i],
+                    'Duration': (pk.GameSeconds - 10 + parseInt(rankArr[i])) * 1000,
+                    'Easing': motoRacing.Easings[num]
+                });
+            }
+            console.log(speeds);
+            return speeds;
         },
+        getRandomNum: function (min, max) {
+            var range = max - min;
+            var rand = Math.random();
+            return (min + Math.round(rand * range));
+        }
     };
 
 });
