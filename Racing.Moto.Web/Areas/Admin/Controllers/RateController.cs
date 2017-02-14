a65bfc654a850b96a6fe882859cecf517149db9d
dayong.wang <wdyday@qq.com>
2017-02-14 17:56:56 +0800

    20170214.1

---
+++ Racing.Moto.Web/Areas/Admin/Controllers/RateController.cs
@@ -0,0 +1,22 @@
+﻿using Racing.Moto.Services.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Racing.Moto.Web.Areas.Admin.Controllers
+{
+    public class RateController : AdminBaseController
+    {
+        #region 赔率设置
+        public ActionResult Setting(int id = 0)
+        {
+            // 0:竞技场, 1: 娱乐场a, 2: 娱乐场b, 3: 娱乐场c
+            ViewBag.RateType = id;
+
+            return View();
+        }
+        #endregion
+    }
+}
