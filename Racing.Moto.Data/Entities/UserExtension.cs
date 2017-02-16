577ac4033497b906fe37dca66155385b04c182a7
dayong.wang <wdyday@qq.com>
2017-02-16 17:52:36 +0800

    20170216.2

--- Racing.Moto.Data/Entities/UserExtension.cs
+++ Racing.Moto.Data/Entities/UserExtension.cs
@@ -24,6 +24,16 @@ namespace Racing.Moto.Data.Entities
         /// </summary>
         public decimal Rebate { get; set; }
 
+        /// <summary>
+        /// 如果当前用户是总代理: 总代理下代理的数量
+        /// </summary>
+        public int AgentCount { get; set; }
+
+        /// <summary>
+        /// 如果当前用户是代理: 代理下会员的数量
+        /// </summary>
+        public int MemberCount { get; set; }
+
         public virtual User User { get; set; }
     }
 }
